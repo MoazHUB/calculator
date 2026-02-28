7109c6613189bf6add74a1662b2c7fb99bb5e098
MoazHUB <moazsherif2004@gmail.com>
2026-03-01 00:05:12 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,29 +1,31 @@
-def addition(firstno,secondno):
-    add=firstno + secondno
-    return add
-def subtraction(firstno,secondno) :
-    minus=firstno - secondno
-    return minus
-def multiplication(fristno,secondno) :
-    multiply=firstno * secondno
-    return multiply
-def division(firstno,secondno) :
+def addition(firstno, secondno):
+    return firstno + secondno
 
-    if secondno==0 :
-        pass
+def subtraction(firstno, secondno):
+    return firstno - secondno
+
+def multiplication(firstno, secondno):
+    return firstno * secondno
+
+def division(firstno, secondno):
+    if secondno == 0:
+        return "Error: Cannot divide by zero"
     else:
-        divide = firstno / secondno
-        return divide
-firstno=int(input('enter the first number'))
-secondno=int(input(' enter the second number'))
-operation=input('enter the function')
-if operation== '+' :
-    print(addition(firstno,secondno))
-elif operation== '-' :
-    print(subtraction(firstno,secondno))
-elif operation== '*' :
-    print(multiplication(firstno,secondno))
-elif operation== '/' :
-    print(division(firstno,secondno))
-else :
-    print('Error 404')
+        return firstno / secondno
+
+# input
+firstno = int(input("Enter the first number: "))
+secondno = int(input("Enter the second number: "))
+operation = input("Enter the operation (+, -, *, /): ")
+
+# operation selection
+if operation == '+':
+    print(addition(firstno, secondno))
+elif operation == '-':
+    print(subtraction(firstno, secondno))
+elif operation == '*':
+    print(multiplication(firstno, secondno))
+elif operation == '/':
+    print(division(firstno, secondno))
+else:
+    print("Error: Invalid operation")
